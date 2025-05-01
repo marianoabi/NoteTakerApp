69b60390503e48a891b27a6734604d6067079f1d
Abigail Mariano <marianoabby@gmail.com>
2025-05-02 00:59:14 +0800

    Initial Commit

---
+++ NoteTakerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteTakerSwiftUI
+//
+//  Created by Abigail Mariano on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteTakerSwiftUI/NoteTakerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakerSwiftUIApp.swift
+//  NoteTakerSwiftUI
+//
+//  Created by Abigail Mariano on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
